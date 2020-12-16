d09bf252b22d265cd968fc27d52bb8b29d6e4e57
Wojciech Gałecki <VuGalPL@gmail.com>
2020-12-16 01:34:17 +0100

    Add acceptance tests files

---
+++ tests/acceptance_tests/iris_scikit_learn_comparison.py
@@ -0,0 +1,3 @@
+# This test will compare the created Naive Bayes classifier implementation
+# with scikit-learn library using iris.csv dataset
+
---
+++ tests/acceptance_tests/pima_indians_diabetes_scikit_learn_comparison.py
@@ -0,0 +1,3 @@
+# This test will compare the created Naive Bayes classifier implementation
+# with scikit-learn library using pima-indians-diabetes.csv dataset
+
